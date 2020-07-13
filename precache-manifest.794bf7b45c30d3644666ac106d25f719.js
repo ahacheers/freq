5e3c3d8f6635343371e78179846e7f0ab5657aba
ahacheers <ahacheers@gmail.com>
2020-07-13 22:21:50 +0200

    Updates

---
+++ precache-manifest.794bf7b45c30d3644666ac106d25f719.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fbe3df6d15657273b31540e6dbe655",
+    "url": "/freq/index.html"
+  },
+  {
+    "revision": "081a82a184c16f22c32c",
+    "url": "/freq/static/css/main.678f25c7.chunk.css"
+  },
+  {
+    "revision": "0c749105a63e51bb6c80",
+    "url": "/freq/static/js/2.2f557415.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/freq/static/js/2.2f557415.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "081a82a184c16f22c32c",
+    "url": "/freq/static/js/main.0766c198.chunk.js"
+  },
+  {
+    "revision": "2ca434183b6270224f99",
+    "url": "/freq/static/js/runtime-main.29f4a5f8.js"
+  }
+]);
